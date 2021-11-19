12ce5d095b6e87b907a2caf616564ccf87c81ba7
GUIDE-WS <IgorGrigorev04@gmail.com>
2021-11-19 16:39:03 +0500

    Steps 5-7

---
+++ filter_with_filename.py
@@ -0,0 +1,25 @@
+import numpy as np
+from PIL import Image
+
+
+def img_to_nparr(path):
+    return np.array(Image.open(path))
+
+
+def save_img_from_nparr(img, name):
+    Image.fromarray(img).save(name)
+
+
+def create_pixel_art(img, size, grayscale):
+    for x in range(0, len(img), size):
+        for y in range(0, len(img[1]), size):
+            brightness = get_average_brightness(img, size, x, y)
+            img[x: x + size, y: y + size] = brightness - brightness % grayscale
+    return img
+
+
+def get_average_brightness(img, size, x, y):
+    return np.average(img[x: x + size, y: y + size])
+
+
+save_img_from_nparr(create_pixel_art(img_to_nparr('img1.jpg'), 10, 50), 'res.jpg')
