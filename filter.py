1d1a3bb1fdf0d330599a8f6a493fdc5f183c3ad2
GUIDE-WS <IgorGrigorev04@gmail.com>
2021-11-19 16:07:29 +0500

    Steps 1-4

---
+++ filter.py
@@ -0,0 +1,28 @@
+import numpy as np
+from PIL import Image
+
+
+def img_to_nparr(path):
+    return np.array(Image.open(path))
+
+
+def save_img_from_nparr(img, name):
+    Image.fromarray(img).save(name)
+
+
+def create_pixel_art(img, size, grayscale):
+    for x in range(0, len(img), size):
+        for y in range(0, len(img[1]), size):
+            brightness = get_average_brightness(img, size, x, y)
+            img[x: x + size, y: y + size] = brightness - brightness % grayscale
+    return img
+
+
+def get_average_brightness(img, size, x, y):
+    return np.average(img[x: x + size, y: y + size])
+
+
+img = img_to_nparr(input('Write the path to the picture: '))
+size = int(input('Write the pixel size: '))
+grayscale = int(input('Write the step of grayscale: '))
+save_img_from_nparr(create_pixel_art(img, size, grayscale), 'res.jpg')
